5a61681b722b4e5181be22dd3eba74d27ce65601
m-j-terry <michaeljterry0815@gmail.com>
2024-03-18 16:38:28 -0400

    create_totals_table ready for testing

--- server.py
+++ server.py
@@ -12,11 +12,43 @@ cursor = conn.cursor()
 ## LOAD DATA INTO A LIST OF CLASS OBJECTS
 corporate_sheets = extract_data()
 
+## QUERY TOTALS TABLE
+def create_totals_table():
+    totals_sheet = "Grand Totals"
+    cursor.execute('CREATE TABLE IF NOT EXISTS {} (id SERIAL PRIMARY KEY, name VARCHAR(30), total INTEGER)'.format(totals_sheet))
+    for corporation in corporate_sheets:
+        ## Add each corporation and sum total waste to Grand Totals table
+        cursor.execute('SELECT SUM(total) AS total_price FROM {}'.format(corporation.name))
+        total_price = cursor.fetchone()[0]
+        cursor.execute('''
+            INSERT INTO {} (name, total) 
+            SELECT ?, ? 
+            WHERE NOT EXISTS (
+                SELECT 1 FROM {} 
+                WHERE name = ? AND total = ?
+            )
+        '''.format(corporation.name, corporation.name), 
+        (corporation.name, total_price, corporation.name, total_price)
+        )
+    ## After adding corporations to Grand Totals, calculate grand total of these.
+    cursor.execute('SELECT SUM(total) AS total_price FROM {}'.format(totals_sheet))
+    total_price = cursor.fetchone()[0]
+    cursor.execute('''
+        INSERT INTO {} (name, total) 
+        SELECT ?, ? 
+        WHERE  NOT EXISTS ( 
+            SELECT 1 FROM {} 
+            WHERE name = ? and total = ?
+        )
+    '''.format(totals_sheet, totals_sheet), 
+    ('Grand Total', total_price))
+    conn.commit()
+
 ## QUERY TABLES
 def create_tables():
     for corporation in corporate_sheets:
-        # cursor.execute('CREATE TABLE IF NOT EXISTS ', corporation.name, '(id SERIAL PRIMARY KEY, date varchar(10), name varchar(45), defects VARCHAR(45), units INTEGER, price INTEGER, total INTEGER)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS {} (id SERIAL PRIMARY KEY, date varchar(10), name varchar(45), defects VARCHAR(45), units INTEGER, price INTEGER, total INTEGER)'.format(corporation.name))
+        # cursor.execute('CREATE TABLE IF NOT EXISTS ', corporation.name, '(id SERIAL PRIMARY KEY, date VARCHAR(10), name VARCHAR(45), defects VARCHAR(45), units INTEGER, price INTEGER, total INTEGER)')
+        cursor.execute('CREATE TABLE IF NOT EXISTS {} (id SERIAL PRIMARY KEY, date VARCHAR(10), name VARCHAR(45), defects VARCHAR(45), units INTEGER, price INTEGER, total INTEGER)'.format(corporation.name))
 
         items = corporation.items
         for item in items:
@@ -28,10 +60,12 @@ def create_tables():
                     SELECT 1 FROM {} 
                     WHERE date = ? AND name = ? AND price = ?
                 )
-            '''.format(corporation.name, corporation.name), (item.date, item.name, item.defects, item.units, item.unit_price, item.total, item.date, item.name, item.unit_price))
+            '''.format(corporation.name, corporation.name), 
+            (item.date, item.name, item.defects, item.units, item.unit_price, item.total, item.date, item.name, item.unit_price))
         conn.commit()
+        create_totals_table()
+    
 create_tables()
-
 
 
 
